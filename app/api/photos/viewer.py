cce3a497b6cdeb5331c9b087928d3809b286ab47
Glauber L. Da Silva <glauber.lucio.silva@gmail.com>
2021-06-21 19:37:25 -0300

    - add method to edit and delete photos - add coments as doc

--- app/api/photos/viewer.py
+++ app/api/photos/viewer.py
@@ -13,6 +13,9 @@ class PhotosList(Resource):
 
     @ns.response(code=200, description="Return a list of photos")
     def get(self):
+        """
+        Return a list of Photos
+        """
         photos = Photo.objects().to_json()
         return make_response(photos, HTTPStatus.OK)
 
@@ -29,9 +32,36 @@ class PhotosList(Resource):
 @ns.param('photo_id', 'The photo identifier')
 class Photo(Resource):
 
-    @ns.doc('get_cat')
+    @ns.doc('get_photo')
     @ns.response(code=200, description="Return a photos")
     def get(self, photo_id):
-        photo = Photo.objects.get(id=photo_id).to_json()
-        return make_response(photo, HTTPStatus.OK)
+        """
+        Retrieves a photo with the entered id
+        :param photo_id:
+        :return:
+        """
+        photo = Photo.objects.get(id=photo_id)
+        return make_response(photo.to_json(), HTTPStatus.OK)
 
+    @ns.doc('update_photo')
+    @ns.response(code=201, description="Update photo information")
+    def put(self, photo_id):
+        """
+        Edit a photo with the entered id
+        :param photo_id:
+        :return:
+        """
+        body = request.json
+        photo = Photo.objects.get(id=photo_id).update(**body)
+        return make_response(photo.to_json(), HTTPStatus.CREATED)
+
+    @ns.doc('delete_photo')
+    @ns.response(code=200, description="Delete a photo")
+    def delete(self, photo_id):
+        """
+        Delete a photo with the entered id
+        :param photo_id:
+        :return:
+        """
+        Photo.objects.get(id=photo_id).delete()
+        return make_response(jsonify({'message': 'Deleted'}), HTTPStatus.OK)
--- app/database/models.py
+++ app/database/models.py
@@ -9,10 +9,3 @@ class Photo(Document):
     approved = BooleanField(default=False, blank=True, null=True)
     created = DateTimeField(auto_now_add=True)
     updated = DateTimeField(auto_now=True)
-
-    # owner = models.ForeignKey('users.User', verbose_name='owner', related_name='photos', on_delete=models.CASCADE)
-    # description = models.CharField('description', max_length=200, blank=True, null=True)
-    # photo_url = models.CharField('photo url', blank=True, null=True, max_length=300)
-    # is_approved = models.BooleanField('is approved', default=False, blank=True, null=True)
-    # created_at = models.DateTimeField('created at', auto_now_add=True)
-    # updated_at = models.DateTimeField('updated at', auto_now=True)

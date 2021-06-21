0313534ca8b94b4ceb99eeaa7375418c87a7ae3e
Glauber L. Da Silva <glauber.lucio.silva@gmail.com>
2021-06-21 20:03:14 -0300

    add meta collection

--- app/database/models.py
+++ app/database/models.py
@@ -3,6 +3,7 @@ from mongoengine import StringField, BooleanField, DateTimeField
 
 
 class Photo(Document):
+    meta = {'collection': 'photo'}
     owner = StringField(required=True)
     description = StringField(max_length=200, blank=True, null=True)
     image_url = StringField(blank=True, null=False)

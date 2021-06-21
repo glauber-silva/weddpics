efd628f64093f2aed932141b1c0a2aa92e7261dd
Glauber L. Da Silva <glauber.lucio.silva@gmail.com>
2021-06-21 12:54:00 -0300

    remove unecessary settings files update docker-compose to build mongo-express

--- app/database/models.py
+++ app/database/models.py
@@ -1,8 +1,18 @@
-from .db import db
+from flask_mongoengine import Document
+from mongoengine import StringField, BooleanField, DateTimeField
 
 
-class Photo(db.Document):
-    description = db.CharField('description', max_length=200, blank=True, null=True)
-    is_approved = db.BooleanField('is approved', default=False, blank=True, null=True)
-    created_at = db.DateTimeField('created at', auto_now_add=True)
-    updated_at = db.DateTimeField('updated at', auto_now=True)
+class Photo(Document):
+    owner = StringField(required=True)
+    description = StringField(max_length=200, blank=True, null=True)
+    image_url = StringField(blank=True, null=False)
+    approved = BooleanField(default=False, blank=True, null=True)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    # owner = models.ForeignKey('users.User', verbose_name='owner', related_name='photos', on_delete=models.CASCADE)
+    # description = models.CharField('description', max_length=200, blank=True, null=True)
+    # photo_url = models.CharField('photo url', blank=True, null=True, max_length=300)
+    # is_approved = models.BooleanField('is approved', default=False, blank=True, null=True)
+    # created_at = models.DateTimeField('created at', auto_now_add=True)
+    # updated_at = models.DateTimeField('updated at', auto_now=True)

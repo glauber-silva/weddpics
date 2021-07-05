08fd8be0135d8193fb28ed49b7874f8cf08f1d65
Glauber L. Da Silva <glauber.lucio.silva@gmail.com>
2021-07-05 02:14:36 -0300

    update manage to use Manager from flask_script instead of cli

--- manage.py
+++ manage.py
@@ -1,15 +1,15 @@
-from flask.cli import FlaskGroup
 from app import create_app
+from flask_script import Manager
 
 app = create_app()
-cli = FlaskGroup(create_app=create_app)
+manager = Manager(app)
 
 
-@cli.command
+@manager.command
 def about():
     return "Wedding photo gallery"
 
 
 if __name__ == '__main__':
-    cli()
+    manager.run()
 
